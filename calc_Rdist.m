39387164683382c4f8f1322623435ce18e07d0f3
hadasbenisty <hadas.benisty@gmail.com>
2023-09-18 15:13:17 +0300

    code upload

--- calc_Rdist.m
+++ calc_Rdist.m
@@ -7,8 +7,12 @@ NN = size(mC, 3);
 r=[];
 for i = 1:NN
     e = sort(eig(mC(:, :, i)), 'descend');
-    r(end+1) = find(e < 0.01, 1)-1;
-
+    t = find(e < 0.01, 1)-1;
+    if isempty(t)
+        r(end+1) = length(e);
+    else
+        e = t;
+    end
 end
 r = min(r);
 
--- calc_centroids.m
+++ calc_centroids.m
@@ -1,5 +1,5 @@
-function mC = calc_centroids(data_all, train_stage)
-
+function [mC, CCall] = calc_centroids(data_all, train_stage)
+CCall = [];
 clusters = unique(train_stage);
 for ci = 1:length(clusters)
     x = data_all(:, 20:end, train_stage == ci);
@@ -9,7 +9,7 @@ for ci = 1:length(clusters)
         e(:,kk) = sort(eig(CC(:,:,kk)), 'descend');
         r(kk) = find(e(:,kk) < median(e(:,kk) ), 1);
     end
-
+    CCall = cat(3, CCall, CC);
     r = min(r);
     mC(:, :, ci) = SpsdMean(CC, r);
 end
